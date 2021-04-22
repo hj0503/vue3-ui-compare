e7964f5c21017844d50a5f4d5da1bde2b5566ffa
hj0503 <857131753@qq.com>
2021-04-23 00:01:50 +0800

    feat: 引入element-plus

--- vue3-element-ui/src/main.ts
+++ vue3-element-ui/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { useElementPlus } from '@/plugins/element-plus'
+
+const app = createApp(App)
+
+useElementPlus(app)
+
+app.mount('#app')
