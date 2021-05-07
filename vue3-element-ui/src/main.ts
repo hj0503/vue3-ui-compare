51cbd7fc3281d69d88102b4fb3f5a89705566bd7
hj0503 <857131753@qq.com>
2021-05-07 22:58:51 +0800

    feat: element-ui布局

--- vue3-element-ui/src/main.ts
+++ vue3-element-ui/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import router from '@/router'
 import App from './App.vue'
+import '@/assets/style.scss'
 
 import { useElementPlus } from '@/plugins/element-plus'
 
