112695bda5118ddf1cd6039ad08384508a28fae0
hj0503 <857131753@qq.com>
2021-06-07 22:15:14 +0800

    feat: vue3-ant-design添加svg-icon组件

---
+++ vue3-ant-design/src/components/svg-icon/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href="symbolId" :fill="color" />
+  </svg>
+</template>
+
+<script lang='ts'>
+import { defineComponent, computed } from 'vue'
+interface SvgIconProps {
+  name: string
+  prefix?: string
+  color?: string
+}
+export default defineComponent({
+  name: 'SvgIcon',
+  props: {
+    prefix: {
+      type: String,
+      default: 'icon',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: '#333',
+    },
+  },
+  setup(props: SvgIconProps) {
+    const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+    return { symbolId }
+  },
+})
+</script>
